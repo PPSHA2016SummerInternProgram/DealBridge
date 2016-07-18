6349429fc1505bd8123cda904c5b19aafb63a303
TJZ1990 <tjz2009@yeah.net>
2016-07-18 10:37:29 +0800

    add discount template

--- db-backend/dealbridge-web/src/main/java/com/paypal/dealbridge/web/controller/DiscountController.java
+++ db-backend/dealbridge-web/src/main/java/com/paypal/dealbridge/web/controller/DiscountController.java
@@ -2,6 +2,7 @@ package com.paypal.dealbridge.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,5 +22,12 @@ public class DiscountController {
 	@ResponseBody
 	public Discount getDiscount(@PathVariable("id") int id) {
 		return discountService.getDiscountById(id);
+	}
+	
+	@RequestMapping(path="/discount/{id}", method=RequestMethod.GET)
+	public String showDiscount(@PathVariable("id") int id, Model model) {
+		Discount discount = discountService.getDiscountById(id);
+		model.addAttribute("discount", discount);
+		return "discount";
 	}
 }
