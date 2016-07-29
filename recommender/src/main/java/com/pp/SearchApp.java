8e05dac753ea016573fad754577b74a433bae6de
chaorshi <chaorshi@paypal.com>
2016-07-29 16:56:34 +0800

    add lucene support for searching

---
+++ recommender/src/main/java/com/pp/SearchApp.java
@@ -0,0 +1,19 @@
+package com.pp;
+
+import org.apache.lucene.analysis.Analyzer;
+
+/**
+ * Created by chaorshi on 2016/7/29.
+ */
+public class SearchApp {
+    private static String INDEX_DIR = "/data/eric/test/lucene-test/index";
+    private static Analyzer analyzer = new MyIKAnalyzer();
+
+    private static void search(String query) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
