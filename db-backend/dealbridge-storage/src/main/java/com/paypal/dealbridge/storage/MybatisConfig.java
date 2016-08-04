270d1e38e58b514201b4a43285b4ad83203f7299
TJZ1990 <tjz2009@yeah.net>
2016-08-04 15:08:24 +0800

    change database address

--- db-backend/dealbridge-storage/src/main/java/com/paypal/dealbridge/storage/MybatisConfig.java
+++ db-backend/dealbridge-storage/src/main/java/com/paypal/dealbridge/storage/MybatisConfig.java
@@ -29,9 +29,8 @@ public class MybatisConfig {
         props.put("username", env.getProperty("jdbc.username"));
         props.put("password", env.getProperty("jdbc.password"));
         props.put("testWhileIdle", "true");
-        props.put("timeBetweenEvictionRunsMillis", "10000");
+        props.put("timeBetweenEvictionRunsMillis", "60000");
         props.put("validationQuery", "SELECT 'x'");
-        props.put("minEvictableIdleTimeMillis", "12000");
         return DruidDataSourceFactory.createDataSource(props);
     }
     
