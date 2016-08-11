ab427988b808b32fbbbbc0ec58cf7be6ca9d3ca7
ffang517 <ffangxyy@126.com>
2016-08-11 18:20:10 +0800

    JUnit test code
    Signed-off-by: ffang517 <ffangxyy@126.com>

--- db-backend/dealbridge-storage/src/test/java/com/paypal/dealbridge/storage/test/SearchHistoryTest.java
+++ db-backend/dealbridge-storage/src/test/java/com/paypal/dealbridge/storage/test/SearchHistoryTest.java
@@ -42,5 +42,23 @@ public class SearchHistoryTest {
 		assertEquals(keyword, histories.get(0));
 	}
 	
+	@Test
+	public void testUpdate() {
+		List<String> histories;
+		SearchHistory record = new SearchHistory();
+		record.setUserId(userId);
+		record.setKeyword(keyword);
+		record.setSearchTime(new Date());
+		searchHistoryMapper.insert(record);
+		
+		histories = searchHistoryMapper.selectUserHistory(userId, 10);
+		assertEquals(1, histories.size());
+		searchHistoryMapper.setInvisible(userId);
+		histories = searchHistoryMapper.selectUserHistory(userId, 10);
+		assertEquals(0, histories.size());
+	}
+	
+	
+	
 
 }
